b932c049ed38d746f4dd3b7ff55ae1f03d1532b6
manuilov_oleg <olegbonder@gmail.com>
2026-01-05 22:39:01 +0300

    1. Подключение react-hook-form, zod 2. Добавление схемы валидации и управление элементами формы в компоненте создания урока

--- frontend/src/features/lessons/create-lesson-dialog.tsx
+++ frontend/src/features/lessons/create-lesson-dialog.tsx
@@ -1,6 +1,3 @@
-import { lessonsApi, lessonsQueryOptions } from "@/entities/lessons/api";
-import { EnvelopeError } from "@/shared/api/errors";
-import { queryClient } from "@/shared/api/query-client";
 import { Button } from "@/shared/components/ui/button";
 import {
   DialogHeader,
@@ -12,41 +9,57 @@ import {
 } from "@/shared/components/ui/dialog";
 import { Input } from "@/shared/components/ui/input";
 import { Label } from "@radix-ui/react-label";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import React, { useState } from "react";
-import { toast } from "sonner";
+import { useState } from "react";
 import useCreateLesson from "./model/use-create-lesson";
+import { z } from "zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const createLessonSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Название урока обязательно")
+    .min(3, "Название должно содержать минимум 3 символа")
+    .max(200, "Название не должно превышать 200 символов"),
+  description: z
+    .string()
+    .min(10, "Описание должно содержать минимум 10 символов")
+    .max(1000, "Описание не должно превышать 1000 символов")
+    .optional(),
+});
+
+type CreateLessonData = z.infer<typeof createLessonSchema>;
 
 type CreateLessonDialogProps = {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 };
 
-type CreateFormData = {
-  title: string;
-  description: string;
-};
-
 export function CreateLessonDialog({
   open,
   onOpenChange,
 }: CreateLessonDialogProps) {
-  const initialData: CreateFormData = {
+  const initialData: CreateLessonData = {
     title: "",
     description: "",
   };
-  const [createFormData, setCreateFormData] =
-    useState<CreateFormData>(initialData);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm<CreateLessonData>({
+    defaultValues: initialData,
+    resolver: zodResolver(createLessonSchema),
+  });
 
   const { createLesson, isPending, error } = useCreateLesson();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("create lesson");
-    createLesson(createFormData, {
+  const onSubmit = async (data: CreateLessonData) => {
+    createLesson(data, {
       onSuccess: () => {
-        setCreateFormData(initialData);
         onOpenChange(false);
+        reset(initialData);
       },
     });
   };
@@ -59,43 +72,47 @@ export function CreateLessonDialog({
             Заполните форму ниже, чтобы создать новый урок.
           </DialogDescription>
         </DialogHeader>
-        <form className="grid gap-4 py-4" onSubmit={(e) => handleSubmit(e)}>
+        <form className="grid gap-4 py-4" onSubmit={handleSubmit(onSubmit)}>
           <div className="grid gap-2">
             <Label htmlFor="title">Название урока</Label>
             <Input
               id="title"
               placeholder="Введите название урока"
-              className="w-full"
-              value={createFormData.title}
-              onChange={(e) =>
-                setCreateFormData({
-                  ...createFormData,
-                  title: e.target.value,
-                })
-              }
+              className={`w-full ${
+                errors.title
+                  ? "border-destructive focus-visible:ring-destructive"
+                  : ""
+              }`}
+              {...register("title")}
             />
+            {errors.title && (
+              <p className="text-sm text-destructive">{errors.title.message}</p>
+            )}
           </div>
           <div className="grid gap-2">
             <Label htmlFor="description">Описание</Label>
             <Input
               id="description"
               placeholder="Введите описание урока"
-              className="w-full"
-              value={createFormData.description}
-              onChange={(e) =>
-                setCreateFormData({
-                  ...createFormData,
-                  description: e.target.value,
-                })
-              }
+              className={`w-full ${
+                errors.description
+                  ? "border-destructive focus-visible:ring-destructive"
+                  : ""
+              }`}
+              {...register("description")}
             />
+            {errors.description && (
+              <p className="text-sm text-destructive">
+                {errors.description.message}
+              </p>
+            )}
           </div>
 
           <DialogFooter>
             <Button variant="outline" onClick={() => onOpenChange(false)}>
               Отмена
             </Button>
-            <Button type="submit" disabled={isPending}>
+            <Button type="submit" disabled={isPending || !isValid}>
               Создать урок
             </Button>
             {error && <div className="text-red-500">{error.message}</div>}
